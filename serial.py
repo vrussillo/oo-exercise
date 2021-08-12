f4a1b16a48041a5e5d8b6f3507a8c7d8680b6e05
vrussillo <80196694+vrussillo@users.noreply.github.com>
2021-08-11 23:05:29 -0700

    This is a new commit for what I originally planned to be amended

---
+++ serial.py
@@ -0,0 +1,38 @@
+"""Python serial number generator."""
+
+class SerialGenerator:
+    """creating an instance of init"""
+    def __init__(self, start=0):
+        self.start = self.next = start
+
+    def __repr__(self):
+        return f"<SerialGenerator start={self.start} next={self.next}>"
+    
+    def generate(self):
+        self.next += 1
+        return self.next - 1
+    
+    def reset(self):
+        self.next = self.start
+    
+
+
+    """Machine to create unique incrementing serial numbers.
+    
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+
